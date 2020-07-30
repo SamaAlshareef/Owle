f69f7eb722ecb69303f0ee8d0a308d7e2b7f18b0
SamaAlshareef <sama.elsherif@student.guc.edu.eg>
2020-07-30 17:39:43 +0200

    Text box font

--- src/Components/aboutUs.jsx
+++ src/Components/aboutUs.jsx
@@ -27,9 +27,11 @@ class AboutUs extends Component {
                 <nav className="navbar navbar-expand-lg navbar-light">
           
                
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+               <button style={{position:'absolute'}} className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
+              
+               
                 <div className="collapse navbar-collapse " id="navbarNavAltMarkup">
                 
                     <div className="navbar-nav ml-auto  ">
