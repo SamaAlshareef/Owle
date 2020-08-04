a3ea47fda764aaae285ebd5c703a3d8989af111c
SamaAlshareef <sama.elsherif@student.guc.edu.eg>
2020-08-04 12:39:59 +0200

    Editing Chatbot

--- src/Components/login.jsx
+++ src/Components/login.jsx
@@ -51,15 +51,7 @@ class Login extends Component {
                 <div className="row  d-flex align-items-center">
                     <div className="col-sm-6">
                         <img className="img-res" src="./images/login/504 [Converted].svg"/>
-                        <div className='d-flex align-items-end' style={{marginTop:'80px'}}>
-                            <img width= '44px'height ='44px' className='img-res' src='./images/home/Mask Group 3.svg'/>
-                            <div className='d-flex align-items-center'>
-                            <div className='chat-box' style={{marginLeft:'20px'}}>
-                                <p className='bot-font' >Hi I'm <strong>Owle Bot</strong>  and click on me if you need any help</p>
-                            </div>
-                            </div>
-                           
-                            </div>
+                       
                     </div>
                     <div className="col-sm-6" style={{marginTop:"35px"}}>
                         <p className="login-title">Welcome Back</p>
@@ -99,7 +91,15 @@ class Login extends Component {
                
                 <div className="row">
                     <div className="col-sm-6">
-
+                    <div className='d-flex align-items-end' style={{marginTop:'80px'}}>
+                            <img width= '44px'height ='44px' className='img-res' src='./images/home/Mask Group 3.svg'/>
+                            <div className='d-flex align-items-center'>
+                            <div className='chat-box' style={{marginLeft:'20px'}}>
+                                <p className='bot-font' >Hi I'm <strong>Owle Bot</strong>  and click on me if you need any help</p>
+                            </div>
+                            </div>
+                           
+                            </div>
                     </div>
                                     <div className="col-sm-6 ">
                                     <p className="logWith">Login With</p>
